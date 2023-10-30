7f622c12b1454046aa85129e56419456a348eb6d
samuel_sanderson <sandersonsamuel401@gmail.com>
2023-10-30 00:50:57 -0300

    Novo Slider com API, Adicionei Bootstrap ao site.

--- script/script.js
+++ script/script.js
@@ -1,19 +1,6 @@
 var radio = document.querySelector('.manual-btn')
 var cont = 1 
-
-setInterval(() => {
-    proxImg()
-}, 5000)
-
-function proxImg(){
-    cont++
-
-    if (cont > 4){
-        cont = 1
-    }
-
-    document.getElementById('radio'+cont).checked = true
-}
+const swiper = document.querySelector('.swiper').swiper;
 
 //ABRINDO E FECHAnDO AS CATEGORIAS
 var nav_categorias = document.getElementById("nav-categorias")
@@ -31,3 +18,33 @@ nav_categorias.addEventListener("mouseleave", function(){
 categorias.addEventListener("mouseleave", function(){
     categorias.style.display = "none"
 })
+
+
+setTimeout(function () {
+    new Swiper('.swiper-container', {
+      // suas opções aqui
+    });
+  }, 1000);
+  
+
+swiper = new Swiper('.swiper', {
+    //Setando a Direção
+    direction: 'horizontal',
+    loop: true,
+  
+    // Setando a paginação
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    //fazendo com que o role com o scroll do mouse
+    mousewheel: {
+        invert: true,
+    },
+  
+    //definindo as setinhas de navegação
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
