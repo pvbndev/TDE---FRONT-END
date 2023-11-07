76178dc547dd8f2e7692fcbdce5dc9b826a7aea8
samuel_sanderson <sandersonsamuel401@gmail.com>
2023-11-07 13:30:15 -0300

    Pegando as informações do produto pelo botão carrinho

--- script/script.js
+++ script/script.js
@@ -19,7 +19,24 @@ categorias.addEventListener("mouseleave", function(){
     categorias.style.display = "none"
 })
 
+function addCart(id){
+  fetch("http://localhost:5000/produtos",{
+  method:"GET",
+  headers:{
+      'Content-type': 'application/json',
+  },
+  }).then((resp) => resp.json())
+  .then((data) =>{
 
+    addProdCart = (data.find((e)=> e.id == id))
+    console.log(addProdCart);
+    
+  })
+
+}
+
+
+//Slider Configuração swiper.js
 setTimeout(function () {
 
   const swiper = new Swiper('.swiper', {
@@ -48,6 +65,8 @@ setTimeout(function () {
 
   }, 1000);
 
+
+//pegando as ofertas do "banco"
 fetch("http://localhost:5000/produtos",{
   method:"GET",
   headers:{
@@ -57,17 +76,22 @@ fetch("http://localhost:5000/produtos",{
 .then((resp) => resp.json())
 .then((data) =>{
 
+  //filtrando apenas as com categoria "Ofertas"
   var produto = (data.find((e)=> e.categProdSec === "Ofertas"))
   var containerOfertas = document.querySelector("#container-ofertas");
 
+  //map para que cada oferta seja imprimida no front
   var prodNomes = data.map(function(produto){
 
+
+    //criando a div que vai comportar os produtos
     var produtos = document.createElement('div');
     produtos.className = "d-flex justify-content-center col-xl-3 col-md-4 pb-5"
 
+    //colocando os elementos concatenados dentro da div
     produtos.innerHTML = `
     <div class="mp-item box-produtos img-thumbnail w-75">
-        <img src="${produto.imagem}" alt="" class="img-thumbnail">
+        <img src="${produto.imagem}" alt="" class="img-thumbnail" id="img-produto">
         <p class="small m-0 text-uppercase">${produto.nome}</p>
         <span class="m-0">
             <div class = "d-flex gap-2">
@@ -77,14 +101,15 @@ fetch("http://localhost:5000/produtos",{
             ${produto.desconto > 0?'<p class="small text-success m-0">' + produto.desconto +'%OFF</p>': '<p class="small text-success m-0">&nbsp</p>'}
         </span>
         <span id="box-comprar">
-            <button class="btn btn-success">Comprar</button>
-            <button class="btn btn-primary">
-                <i class="fa-solid fa-cart-shopping"></i>
-            </button>
+          <button class="btn btn-success">Comprar</button>
+          <button class="btn btn-primary" onclick="addCart(${produto.id})">
+              <i class="fa-solid fa-cart-shopping"></i>
+          </button>
         </span>
     </div>
     `;
 
+    //colocando a div filha dentro da div pai que foi pega anteriormente  
     containerOfertas.appendChild(produtos);
 
   })
